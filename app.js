9d4ba1f3ec95b181c481d8ba8a4c8fe94f1095b8
Yating(Esther) Su <yaringsu@foxmail.com>
2019-07-15 14:29:40 -0400

    add js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var apiKey = "PipsOGUzO5gh4GVMKIuvbFdYnvpY9pNS";
 var filter = "";
-var articalURL = "https://api.nytimes.com/svc/search/v2//articlesearch.json?q=titanic&fq=" + filter +"&api-key=" + apiKey;
+//var articalURL = "https://api.nytimes.com/svc/search/v2//articlesearch.json?q=titanic&fq=" + filter +"&api-key=" + apiKey;
 var queryURL = "";
 
+    startYear = $("#startYear").val();
+    endYear = $("#endYear").val();
+    for(let i = (startYear*1); i < (endYear *1); i++){
+        filter += `"` + i + `"`+ "%20";
+    }
+    console.log(filter);
+
+var pubYear = "pub_year:(" + filter + ")";
+var articalURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=titanic&fq=" + pubYear +"&api-key=" + apiKey;
+console.log(articalURL);
 $.ajax({
     url: articalURL,
     method:"GET"
