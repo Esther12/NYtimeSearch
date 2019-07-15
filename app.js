3a576eeff4290bc06d36a5c69035d15a283c472b
Yating(Esther) Su <yaringsu@foxmail.com>
2019-07-15 16:03:23 -0400

    make it functional and add default value to the table

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
 var apiKey = "PipsOGUzO5gh4GVMKIuvbFdYnvpY9pNS";
 var filter = "";
+var pubYear;
+var query = "";
 //var articalURL = "https://api.nytimes.com/svc/search/v2//articlesearch.json?q=titanic&fq=" + filter +"&api-key=" + apiKey;
 var queryURL = "";
 $("#submitForm").on("click",function(){
-    debugger;
+    // debugger;
     var startYear = $("#startYear").val();
     var endYear = $("#endYear").val();
-    for(let i = (startYear*1); i < (endYear *1); i++){
-        filter += `"` + i + `"`+ "%20";
+    var query = $("#searchTerm").val();
+    if(startYear.trim() == "" && endYear.trim() == ""){
+        pubYear ="";
+    }else{
+        for(let i = (startYear*1); i < (endYear *1); i++){
+            filter += `"` + i + `"`+ "%20";
+        }
+        console.log(filter);
+    
+        pubYear = "pub_year:(" + filter + ")";
     }
-    console.log(filter);
 
-var pubYear = "pub_year:(" + filter + ")";
-var articalURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=titanic&fq=" + pubYear +"&api-key=" + apiKey;
+var articalURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ query.trim() +"&fq=" + pubYear.trim() +"&api-key=" + apiKey;
 console.log(articalURL);
 $.ajax({
     url: articalURL,
     method:"GET"
 }).then(function(response){
-
-        console.log(response);
+    var html = "";
+    var data = response.response.docs;
+    var displayNum = $("#num").val();
+    console.log(`.. about to dsplay ${displayNum} entries for`, data );
+    let numberofData = 0;
+    if(displayNum == 0){
+        numberofData = 20;
+    }
+    else{
+        numberofData = displayNum;
+    }
+    for(let i = 0; i < numberofData; i++){
+        html += `<tr>`+
+                `<th scope = "row"> ${i+1} </th>`+
+                `<td>${data[i].headline.main}</td>`+
+                `<td>${data[i].byline.original}</td>`+
+                `</tr>`;
+    }
+    $("#dataTable").append(html);
+        
 })
 
 })
